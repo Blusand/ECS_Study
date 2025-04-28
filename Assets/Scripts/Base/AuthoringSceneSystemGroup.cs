3383b3f0a98bc57bb5805b080715f6919dde7647
Blusand <1041330105@qq.com>
2025-04-28 15:03:13 +0800

    [Feature] 创建项目

---
+++ Assets/Scripts/Base/AuthoringSceneSystemGroup.cs
@@ -0,0 +1,43 @@
+using Unity.Entities;
+using Unity.Scenes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Entities.Base
+{
+    public abstract partial class AuthoringSceneSystemGroup : ComponentSystemGroup
+    {
+        private bool m_Initialized;
+
+        protected abstract string AuthoringSceneName { get; }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            m_Initialized = false;
+        }
+
+        protected override void OnUpdate()
+        {
+            if (!m_Initialized)
+            {
+                if (SceneManager.GetActiveScene().isLoaded)
+                {
+                    var subScene = Object.FindObjectOfType<SubScene>();
+                    if (subScene != null)
+                    {
+                        Enabled = AuthoringSceneName == subScene.gameObject.scene.name;
+                    }
+                    else
+                    {
+                        Enabled = false;
+                    }
+
+                    m_Initialized = true;
+                }
+            }
+
+            base.OnUpdate();
+        }
+    }
+}
